f156136a474eaed6952f780d6f7170c0af9c0f69
plake <plake492@gmail.com>
2019-11-16 07:33:52 -0500

    random word now dased, working on finding logic

--- src/components/WordBox.js
+++ src/components/WordBox.js
@@ -1,17 +1,50 @@
 import React, { Component } from "react";
 import words from "../utilities/Words.json";
 
+const randomize = Math.floor(Math.random() * words.length); //? generates random word from word list //
+const wordDashedArr = [];
+const indexNum = 0;
+
 class WordBox extends Component {
   state = {
     lettersGuessed: [],
-    randomWord: ""
+    randomWord: [],
+    wordDashed: ""
+  };
+
+  addDashes = word => {
+    word.forEach(x => {
+      if (x !== " " && x !== "!" && x !== "?" && x !== ".") {
+        wordDashedArr.push(x.replace(x, "-"));
+      } else {
+        wordDashedArr.push(x);
+      }
+    });
+    this.setState({ wordDashed: wordDashedArr });
   };
 
   pickRandomWord = () => {
     words.forEach(x => {
-      if (x.id === Math.floor(Math.random() * words.length)) {
-        this.setState({ randomWord: x.word });
+      if (x.id === randomize) {
+        this.setState({ randomWord: x.word.toLowerCase().split("") });
+        this.addDashes(x.word.split("")); //? turns random word into dashes
       }
+    });
+  };
+
+  handleKeyUp = event => {
+    this.state.randomWord.forEach(x => {
+      if (x === event.key) {
+        console.log(this.state.randomWord.indexOf(x));
+        // console.log("That letter is correct", x.indexOf(this.state.randomWord));
+      }
+    });
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
     });
   };
 
@@ -21,9 +54,11 @@ class WordBox extends Component {
 
   render() {
     console.log(this.state.randomWord);
+    console.log(this.state.wordDashed);
     return (
       <div>
-        <h1>{this.state.randomWord}</h1>
+        <h1>{this.state.wordDashed}</h1>
+        <input type="text" ref="letterGuess" onKeyUp={this.handleKeyUp} />
       </div>
     );
   }
